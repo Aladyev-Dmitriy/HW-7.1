e423d87c909ce69f2b60e547703681b1031afc97
Aladyev Dmitriy <aladyev.dmitriy@gmail.com>
2022-07-27 14:02:12 +0300

    Initial commit

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatsService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StatsServiceTest {
+
+
+    @Test
+    public void sumTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSalesTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSales(sales);
+        long expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSalesTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countLowSalesTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.countLowSales(sales);
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countHighSalesTest() {
+        StatsService service = new StatsService();
+        long sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.countHighSales(sales);
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
